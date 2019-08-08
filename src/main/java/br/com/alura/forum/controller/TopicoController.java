9c762a7e28fb66319da476119ad918835ba60c25
Quaresma <bianca.f.quaresma@accenture.com>
2019-08-08 15:59:25 -0300

    implementação de metodo de busca por ID

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,5 +56,13 @@ public class TopicoController {
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 	
+	@GetMapping("/{id}")
+	public TopicoDto detalhar(@PathVariable Long id) {
+		Topico topico = topicoRepository.getOne(id);
+		return new TopicoDto(topico);
+		
+		
+		
+	}
 
 }
